29707dd6649b1db1f437ae5af813d44f9b43147f
syedarshmusharraf <syedme23019@gmail.com>
2025-07-17 22:30:49 +0530

    first commit

--- src/square.java
+++ src/square.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
-public class area {
+
+public class square {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
-        int area = a *a;
+        int square = a * a;
         System.out.println("the square is :" + square);
     }
 }
